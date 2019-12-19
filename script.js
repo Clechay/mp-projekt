4333d26d64e42010acfb7f076beb22be3419777c
Clechay <luke.clechay@gmail.com>
2019-12-19 18:30:14 +0100

    fix bugs

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ const ui = document.querySelector("#ui");
 /**
  * @type {HTMLCanvasElement}
  */
-const canvas = document.querySelector("#game");
+const canvas = document.querySelector("#board");
 const ctx = canvas.getContext("2d");
